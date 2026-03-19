0711a4e92e27ced168e51abc582a80de608a9be9
dmols1 <889010ols@ames.k12.ia.us>
2026-03-19 18:19:36 -0500

    Add files via upload

--- jugglefiles/script.js
+++ jugglefiles/script.js
@@ -1,18 +1,21 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 let mouse = {x: 0, y: 0};
+let maxDragHeight = 200;
+let dragging = null;
+let tick = 0;
+let timer = 0;
 
 class Ball{
     static allInstances = [];
     constructor(x = 0, y = 0){
         this.positionX = x;
         this.positionY = y;
-        this.scale = 60;
+        this.scale = 40;
         this.velocityX = 0;
-        this.velocityY = 5;
-        this.maxSpeed = 8;
-        this.gravityInfluence = 0.15;
-        this.isDragging = false;
+        this.velocityY = 0;
+        this.maxSpeed = 40;
+        this.gravityInfluence = 0.4;
         this.dragOffsetX = 0;
         this.dragOffsetY = 0;
         this.img = new Image();
@@ -28,38 +31,54 @@ class Ball{
         }if(y > 600-this.scale/2){
             this.positionY = 600-this.scale/2;
             this.updateVelocity(this.velocityX, 0);
+        }if(y <= maxDragHeight && this.velocityY < 0){
+            this.maxSpeed = 4;
+        }else{
+            this.maxSpeed = 40;
         }
     }
     updateVelocity(x, y){
-        this.velocityX = Math.min(Math.max(x, -this.maxSpeed), this.maxSpeed);
-        this.velocityY = Math.min(Math.max(y, -this.maxSpeed), this.maxSpeed);
+        let direction = Math.atan2(y,x);
+        let scalar = Math.min(Math.sqrt(x**2 + y**2), this.maxSpeed);
+        this.velocityX = Math.cos(direction)*scalar;
+        this.velocityY = Math.sin(direction)*scalar;
     }
 }
 
-new Ball(200, 400);
-new Ball(100, 350);
-new Ball(250, 300);
-new Ball(50, 250);
+new Ball(150, 500);
+ctx.font = "30px Verdana";
+ctx.textAlign = "center";
 setInterval(run, 1000/60);
 
 function run(){
+    tick++;
     Ball.allInstances.forEach(ball => {
-        if(ball.isDragging){
-            ball.updateVelocity(mouse.x - ball.positionX + ball.dragOffsetX, mouse.y - ball.positionY + ball.dragOffsetY);
+        if(dragging == ball){
+            ball.updateVelocity((mouse.x - ball.positionX + ball.dragOffsetX)/2, (mouse.y - ball.positionY + ball.dragOffsetY)/2);
             ball.updatePosition(mouse.x + ball.dragOffsetX, mouse.y + ball.dragOffsetY);
         }else{
             ball.updatePosition(ball.positionX + ball.velocityX, ball.positionY + ball.velocityY);
             ball.updateVelocity(ball.velocityX, ball.velocityY - ball.gravityInfluence);
         }
     });
+    if(tick % 60 == 0){
+        timer++;
+        timer % 10 == 0 && new Ball(150, 500);
+    }
     draw();
 }
 
 function draw(){
     ctx.clearRect(0, 0, 300, 600);
+    ctx.fillStyle = "#222255";
+    ctx.fillRect(0, 0, 300, 600);
+    ctx.fillStyle = "#4444aa";
+    ctx.fillRect(0, 600-maxDragHeight, 300, maxDragHeight);
     Ball.allInstances.forEach(ball => {
         ctx.drawImage(ball.img, ball.positionX-ball.scale/2, 600-ball.positionY-ball.scale/2, ball.scale, ball.scale);
     });
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(timer, 150, 50);
 }
 
 function mousePos(c, e){
@@ -74,29 +93,25 @@ function mousePos(c, e){
 function mouseDown(event){
     mouse = mousePos(canvas, event);
     Ball.allInstances.forEach(ball => {
-        if(mouse.x >= ball.positionX - ball.scale/2 && mouse.x <= ball.positionX + ball.scale/2 &&
-        mouse.y >= ball.positionY - ball.scale/2 && mouse.y <= ball.positionY + ball.scale/2){
-            ball.isDragging = true;
+        if(dragging == null && mouse.x >= ball.positionX - ball.scale/2 - 10 && mouse.x <= ball.positionX + ball.scale/2 + 10 &&
+        mouse.y >= ball.positionY - ball.scale/2 - 10 && mouse.y <= ball.positionY + ball.scale/2 + 10 && mouse.y <= maxDragHeight){
+            dragging = ball;
             ball.updateVelocity(0, 0);
             ball.dragOffsetX = ball.positionX - mouse.x;
             ball.dragOffsetY = ball.positionY - mouse.y;
         }
     });
-    event.preventDefault();
 }
 
-function mouseUp(event){
-    Ball.allInstances.forEach(ball => {
-        if(ball.isDragging){
-            ball.isDragging = false;
-        }
-    });
-    event.preventDefault();
+function mouseUp(){
+    dragging = null;
 }
 
 function mouseMove(event){
     mouse = mousePos(canvas, event);
-    event.preventDefault();
+    if(mouse.y >= maxDragHeight){
+        mouseUp();
+    }
 }
 
 canvas.addEventListener("pointerdown", mouseDown);
